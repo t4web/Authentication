ab83523a1dd47cbfde5a7a6578dc7ef5f01c4924
maxgu <maxgu@t4web.com.ua>
2014-09-02 08:15:18 +0300

    update composer.json

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Authentication;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
