824d720ccb64c497b4fdb039782f8e57c69531f6
maxgu <maxgu@t4web.com.ua>
2014-09-06 10:23:56 +0300

    add login-form view

--- config/module.config.php
+++ config/module.config.php
@@ -1,7 +1,13 @@
 <?php
 
 return array(
-    
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'authentication' => __DIR__ . '/../view',
+        ),
+    ),
+
     'router' => array(
         'routes' => array(
             'auth-login' => array(
