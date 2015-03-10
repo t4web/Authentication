b2d1ef909da3384489c66155b5aec506b746f904
maxgu <maxgu@t4web.com.ua>
2015-03-10 18:39:04 +0200

    redirect from anywhere when unauthorized

--- Module.php
+++ Module.php
@@ -1,16 +1,64 @@
 <?php
+
 namespace Authentication;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ControllerProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\Mvc\Controller\ControllerManager;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\MvcEvent;
+use Zend\EventManager\EventInterface;
+use Zend\Mvc\Router\RouteMatch;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface, ControllerProviderInterface,
-                        ServiceProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
+                        ControllerProviderInterface, ServiceProviderInterface,
+                        BootstrapListenerInterface
 {
+    public function onBootstrap(EventInterface $e)
+    {
+        $app = $e->getApplication();
+        $em  = $app->getEventManager();
+        $sm  = $app->getServiceManager();
+
+        $auth = $sm->get('Authentication\Service');
+
+        $em->attach(MvcEvent::EVENT_ROUTE, function($e) use ($auth) {
+            $match = $e->getRouteMatch();
+
+            // No route match, this is a 404
+            if (!$match instanceof RouteMatch) {
+                return;
+            }
+
+            // Route is whitelisted
+            $name = $match->getMatchedRouteName();
+            if ($name == 'auth-login') {
+                return;
+            }
+
+            // User is authenticated
+            //if ($auth->hasIdentity()) {
+            //   return;
+            //}
+
+            // Redirect to the user login page, as an example
+            $router   = $e->getRouter();
+            $url      = $router->assemble(array(), array(
+                'name' => 'auth-login'
+            ));
+
+            $response = $e->getResponse();
+            $response->getHeaders()->addHeaderLine('Location', $url);
+            $response->setStatusCode(302);
+            $response->setReasonPhrase('Unauthorized');
+
+            return $response;
+        }, -100);
+    }
+
     public function getConfig($env = null)
     {
         return include __DIR__ . '/config/module.config.php';
