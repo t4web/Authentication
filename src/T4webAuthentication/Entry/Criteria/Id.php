1affefb68d55ce7400fff3d7433b38ff9c194357
maxgu <maxgu@t4web.com.ua>
2015-03-26 22:25:35 +0200

    add Id criteria

---
+++ src/T4webAuthentication/Entry/Criteria/Id.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace T4webAuthentication\Entry\Criteria;
+
+use T4webBase\Domain\Criteria\Id as BaseIdCriteria;
+
+class Id extends BaseIdCriteria {
+    
+    protected $table = 'auth';
+}
