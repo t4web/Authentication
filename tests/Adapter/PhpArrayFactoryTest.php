f9e68d866357781c42e96e07dd55fcb67722ce90
maxgu <gulturyan@t4web.com.ua>
2016-02-18 12:10:50 +0200

    add Adapter\phpArrayFactory test

---
+++ tests/Adapter/PhpArrayFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace T4web\AuthenticationTest\Adapter;
+
+use Zend\ServiceManager\ServiceLocatorInterface;
+use T4web\Authentication\Adapter\PhpArray;
+use T4web\Authentication\Adapter\PhpArrayFactory;
+
+class PhpArrayFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    private $serviceLocator;
+
+    public function setUp()
+    {
+        $this->serviceLocator = $this->prophesize(ServiceLocatorInterface::class);
+        $config = [
+            'auth-accounts' => [
+                'admin' => '111',
+            ]
+        ];
+        $this->serviceLocator->get('Config')->willReturn($config);
+    }
+
+    public function testAuthenticate()
+    {
+        $factory = new PhpArrayFactory();
+
+        $adapter = $factory->createService($this->serviceLocator->reveal());
+
+        $this->assertTrue($adapter instanceof PhpArray);
+    }
+}
